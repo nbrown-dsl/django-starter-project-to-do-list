70bec4ab7c54e63e7a73af9114fc17fb4213ef03
nick brown <nick@browndesign.co.uk>
2020-05-05 13:40:23 +0100

    tweaks

--- my_app/todo_list/urls.py
+++ my_app/todo_list/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home',views.home,name='home'),
     path('',views.home,name='home'),
     path('about',views.about,name='about'),
     path('delete/<list_id>',views.delete,name='delete'),
