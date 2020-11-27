08b9037109522fdccc184e189ded6489056ff8d4
nbrown <nbrown@dwightlondon.org>
2020-11-27 14:38:45 +0000

    more comments

--- my_app/todo_list/models.py
+++ my_app/todo_list/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+# CREATE DATABASE MIGRATION: python manage.py makemigrations
+# MIGRATE THE DATABASE: python manage.py migrate #
+# use in terminal from 'todo_list.models import List' to query list table #
 
-# create databse table with attributes.
+# create database table with attributes.
+
 class List(models.Model):
     item =  models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
