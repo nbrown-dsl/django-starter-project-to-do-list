06f90f481449b4a602635e9b9955d9070230064b
nbrown-dsl <nbrown@dwightlondon.org>
2021-01-12 13:55:10 +0000

    databse protocol schema created in models.py

--- my_app/todo_list/models.py
+++ my_app/todo_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 from django.forms.forms import Form 
 
 # CREATE DATABASE MIGRATION: python manage.py makemigrations
@@ -32,4 +33,29 @@ class List(models.Model):
         return self.item + ' completed: ' + str(self.completed)
 
 
+class protocol_type(models.Model):
+    description =  models.CharField(max_length=250,default='') 
+    name =  models.CharField(max_length=100,)
+
+class protocol(models.Model):
+    type = models.ForeignKey(protocol_type,on_delete=models.DO_NOTHING)
+    form = models.OneToOneField(List,on_delete=models.CASCADE)
+
+class protocol_field(models.Model):
+    field = models.ForeignKey(List,on_delete=models.CASCADE)
+    type = models.ForeignKey(protocol_type,on_delete=models.CASCADE)
+
+class task(models.Model): 
+    description =  models.CharField(max_length=250,default='') 
+    protocolType = models.ForeignKey(protocol_type,on_delete=models.DO_NOTHING)
     
+class task_Data(models.Model):
+    completed = models.BooleanField(default=False)
+    completionDate = models.DateField(null=True)
+    notes =  models.CharField(max_length=250,default='')
+    protocol = models.ForeignKey(protocol,on_delete=models.CASCADE)
+    task = models.ForeignKey(task,on_delete=models.CASCADE)
+
+class responsibility(models.Model):
+    task = models.ForeignKey(task,on_delete=models.CASCADE)
+    person = models.ForeignKey(persons,on_delete=models.CASCADE) 
