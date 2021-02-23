765899091f231dcf951202d478bb136dabef9171
nbrown-dsl <nbrown@dwightlondon.org>
2021-02-23 11:32:07 +0000

    procfile gunicorn django-heroku added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django-starter-project-to-do-list.wsgi
--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -162,3 +162,6 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
