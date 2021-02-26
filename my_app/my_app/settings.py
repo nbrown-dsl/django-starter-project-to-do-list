e5f3749b059f2074853a49e28fad74fffa70a723
nbrown-dsl <nbrown@dwightlondon.org>
2021-02-26 08:36:54 +0000

    stack code moved below debug

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -14,6 +14,22 @@ import os
 from decouple import config
 import dj_database_url
 
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 #stackoverflow code
 # https://stackoverflow.com/questions/66370495/heroku-build-of-django-app-successful-but-open-app-throws-error
 
@@ -35,19 +51,7 @@ else:   # Running on Heroku
 
 #stock overflow end 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 
 ALLOWED_HOSTS = []
 
