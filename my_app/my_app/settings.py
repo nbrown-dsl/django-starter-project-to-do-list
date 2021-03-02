986d751c69c4ce2a0dc484ee26e1dc519a01f0b8
nbrown-dsl <nbrown@dwightlondon.org>
2021-03-02 13:46:03 +0000

    default sqlite

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -36,26 +36,26 @@ LOGIN_REDIRECT_URL = '/'
 
 DATABASES = {
     #default sqlite
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# #postgres
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'protocol',
-
-        'USER': 'admin',
-
-        'PASSWORD': '1234',
-
-        'HOST': 'localhost',
-
-        'PORT': '',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+# # #postgres
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'protocol',
+
+#         'USER': 'admin',
+
+#         'PASSWORD': '1234',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '',
+
+    # }
 
 }
 
