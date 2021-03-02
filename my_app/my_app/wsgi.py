5c263cbd5d784dcf05af6fe054689938410b0e71
nbrown-dsl <nbrown@dwightlondon.org>
2021-03-02 15:10:50 +0000

    working

--- my_app/my_app/wsgi.py
+++ my_app/my_app/wsgi.py
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_app.settings')
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
--- my_app/todo_list/urls.py
+++ my_app/todo_list/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')), # <--
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('logout', views.logout_request, name='logout'),
     path('home',views.home,name='home'),
