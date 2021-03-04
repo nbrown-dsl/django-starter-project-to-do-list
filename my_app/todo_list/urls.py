ba251685f9692167036a620d9909ff6ab4840545
nbrown-dsl <nbrown@dwightlondon.org>
2021-03-04 12:00:05 +0000

    home as landing page

--- my_app/todo_list/urls.py
+++ my_app/todo_list/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')), # <--
+    path('', views.home,name='home'), # <--
     # path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('logout', views.logout_request, name='logout'),
