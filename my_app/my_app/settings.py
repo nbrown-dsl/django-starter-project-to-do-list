09a0c963c528dd9b73e5e5ffd6cf5e8341bf204a
nbrown-dsl <nbrown@dwightlondon.org>
2021-03-05 15:36:31 +0000

    hmm

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = 'my_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
