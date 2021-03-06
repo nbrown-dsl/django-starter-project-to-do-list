f9c20b30ff34f19173cfb64cbcc536c1601f82d7
nbrown-dsl <nbrown@dwightlondon.org>
2021-03-06 09:12:37 +0000

    hmm

--- my_app/my_app/urls.py
+++ my_app/my_app/urls.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
+# from django.contrib.auth import views
 
 
 urlpatterns = [
     path('',include('todo_list.urls')),
     path('admin/', admin.site.urls),
-    path('login/',views.LoginView)
+    # path('login/',views.LoginView)
     
 ]
