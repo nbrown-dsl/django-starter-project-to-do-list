40077717d027efed8e4e28caab55855f47747bf5
nbrown-dsl <nbrown@dwightlondon.org>
2021-11-03 14:40:36 +0000

    gdfgdfs

--- .venvLibsite-packages/gsheets/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AccessCredentials
-
-
-@admin.register(AccessCredentials)
-class AccessCredentialsAdmin(admin.ModelAdmin):
-    fields = ('token', 'refresh_token', 'token_uri', 'scopes', 'created_time',)
-    readonly_fields = ('token_uri', 'scopes', 'created_time')
