062b1c80d2e6868bfdf44f54ceaab789789b53c3
nbrown-dsl <nbrown@dwightlondon.org>
2021-11-10 14:29:23 +0000

    blah

--- quickstart.py
+++ quickstart.py
@@ -5,7 +5,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-from my_app.competencies.models import *
+
 
 
 
